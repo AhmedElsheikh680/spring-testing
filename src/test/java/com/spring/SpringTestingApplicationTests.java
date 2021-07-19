c5c51eee638a4ebaa40787ea5cc811ca9450a854
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-07-19 14:54:30 +0200

    TDD #2 cmpare between two objects

--- src/test/java/com/spring/SpringTestingApplicationTests.java
+++ src/test/java/com/spring/SpringTestingApplicationTests.java
@@ -10,8 +10,11 @@ class SpringTestingApplicationTests {
     @Test
     void namePlayerTest() {
 
-        Player player = new Player();
-        Assertions.assertEquals(player.getName("Trka"), null);
+//        Player player = new Player();
+//        Assertions.assertEquals(player.getName("Trka"), null);
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Assertions.assertEquals(player1.getName("Treka"), player2.getName("Treka"));
     }
 
 }
