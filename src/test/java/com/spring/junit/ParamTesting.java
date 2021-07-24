8171df0bf487c32c9e935bf11156dc18d29f362f
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-07-24 11:09:27 +0200

    @CsvSource()

--- src/test/java/com/spring/junit/ParamTesting.java
+++ src/test/java/com/spring/junit/ParamTesting.java
@@ -3,6 +3,7 @@ package com.spring.junit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -21,5 +22,20 @@ public class ParamTesting {
     @EnumSource(Gender.class)
     void enumTest(Gender value) {
         System.out.println(value);
+    }
+
+
+    @DisplayName("Param Test 2")
+    @ParameterizedTest(name = "{displayName}: {index} => {arguments}")
+    @CsvSource({
+            "Ahmed, 25, 015",
+            "Ali, 20, 011",
+            "Samy, 45, 010"
+    })
+    void paramTest(String name, int age, String phone){
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+        System.out.println("Phone: "+ phone);
+
     }
 }
